fccc5091426e03a4f721de6c692b8d05f981b2c5
Naina.Upadhyay <nanu02d8@gmail.com>
2022-09-18 21:46:47 +0530

    Add login form and home component

--- TOC_Week2/script.js
+++ TOC_Week2/script.js
@@ -4,11 +4,24 @@ let searchBtn = document.getElementById("search-btn");
 let cartBtn = document.getElementById("cart-btn");
 let shoppingCart = document.getElementsByClassName("shopping-cart");
 
+let loginBtn = document.getElementById("login-btn");
+let loginForm = document.getElementsByClassName("login-form");
+
 searchBtn.addEventListener("click", () => {
     console.log(searchForm);
     searchForm[0].classList.toggle("active");
+    shoppingCart[0].classList.remove("active");
+    loginForm[0].classList.remove("active");
 });
 
 cartBtn.addEventListener("click", () => {
-    shoppingCart[0].classList.toggle("showCart");
+    shoppingCart[0].classList.toggle("active");
+    searchForm[0].classList.remove("active");
+    loginForm[0].classList.remove("active");
+})
+
+loginBtn.addEventListener("click", () => {
+    loginForm[0].classList.toggle("active");
+    shoppingCart[0].classList.remove("active");
+    searchForm[0].classList.remove("active");
 })
